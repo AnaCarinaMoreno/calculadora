bf4d3f12c631f297824ac8a3bbb588baf93a26de
Carina <anacarina.morenodiaz@gmail.com>
2022-12-11 10:42:09 -0500

    calculadora básica con React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React from 'react'
+import Boton from './components/Boton';
+import Pantalla from './components/Pantalla';
 import './App.css';
+import BotonClear from './components/BotonClear';
+import { useState } from 'react';
+import {evaluate} from 'mathjs';
 
 function App() {
+    const [input, setInput] = useState('');
+
+    const adicionarInput = val =>{
+      setInput(input + val)
+
+    };
+
+    const calcular = () => {
+      if (input){
+        setInput(evaluate(input))
+      }
+       else {
+        alert('Por favor ingrese valores pra realizar los cálculos')
+       }
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className = "cont-calculadora">
+         <Pantalla input= {input} />
+         <div className = "fila">
+          <Boton manejarClick = {adicionarInput}>1</Boton>
+          <Boton manejarClick = {adicionarInput}>2</Boton>
+          <Boton manejarClick = {adicionarInput}>3</Boton>
+          <Boton manejarClick = {adicionarInput}>+</Boton>
+         </div>
+         <div className = "fila">
+          <Boton manejarClick = {adicionarInput}>4</Boton>
+          <Boton manejarClick = {adicionarInput}>5</Boton>
+          <Boton manejarClick = {adicionarInput}>6</Boton>
+          <Boton manejarClick = {adicionarInput}>-</Boton>
+         </div>
+         <div className = "fila">
+          <Boton manejarClick = {adicionarInput}>7</Boton>
+          <Boton manejarClick = {adicionarInput}>8</Boton>
+          <Boton manejarClick = {adicionarInput}>9</Boton>
+          <Boton manejarClick = {adicionarInput}>*</Boton>
+         </div>
+         <div className = "fila">
+          <Boton manejarClick = {calcular}>=</Boton>
+          <Boton manejarClick = {adicionarInput}>0</Boton>
+          <Boton manejarClick = {adicionarInput}>.</Boton>
+          <Boton manejarClick = {adicionarInput}>/</Boton>
+         </div>
+         <div className = "fila">
+            <BotonClear manejarClear = {() => setInput('')}>
+              Clear
+            </BotonClear>
+         </div>
+
+
+       </div>
     </div>
   );
 }
